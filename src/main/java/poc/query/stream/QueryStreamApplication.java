8b4c16c536841c773451d907da6622479e6308f8
charandoli <kali.dkc@gmail.com>
2025-05-28 00:37:45 -0400

    Add Spring boot support

---
+++ src/main/java/poc/query/stream/QueryStreamApplication.java
@@ -0,0 +1,13 @@
+package poc.query.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/query/stream/QueryStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.query.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
